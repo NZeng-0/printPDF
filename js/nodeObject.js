34263f85b41cbecd72a40d64cb59ebf7ec273ed2
Zeng <3243916866@qq.com>
2023-02-15 22:27:40 +0800

    2023/02/15完成部分

---
+++ js/nodeObject.js
@@ -0,0 +1,10 @@
+function Element (tagName, props, children) {
+    this.tagName = tagName
+    this.props = props
+    this.children = children
+  }
+  
+  module.exports = function (tagName, props, children) {
+    return new Element(tagName, props, children)
+  }
+  
